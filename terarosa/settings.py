57aa0e12c56f70fcc07bc27f9225e8db3a51ef41
kdylsky <kdy9kdy@gmail.com>
2022-11-16 21:55:04 +0900

    [ADD] 로그인 데코레이터 추가

--- terarosa/settings.py
+++ terarosa/settings.py
@@ -37,8 +37,8 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    # 'django.contrib.admin',
-    # 'django.contrib.auth',
+    'django.contrib.admin',
+    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -56,11 +56,10 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
-    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-
 ]
 
 ROOT_URLCONF = 'terarosa.urls'
--- users/views.py
+++ users/views.py
@@ -1,27 +1,17 @@
-import bcrypt
-import jwt
-import json
-
-from django.conf import settings
-
-from core.utils import *
-
-from rest_framework.views import APIView
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import CreateModelMixin
 from rest_framework.decorators import parser_classes, api_view
 from rest_framework.parsers import JSONParser
 from rest_framework import status
 from django.http import JsonResponse
 from users.service import UserService
 from users.serializers import UserModelSerializer, LoginSchema
-
+from decorators.execption_handler import execption_hanlder
 # content_type을 확인해서 request.data에 담아준다는 의미이다.?
 # parser클래스를 정의함으로써 내가 원하느content_type이 아닌 경우에는 오류가 발생?
 # 들어오는 요청의 헤더를 검사한다.
 
 user_service = UserService()
 
+@execption_hanlder()
 @api_view(['POST'])
 @parser_classes([JSONParser])
 def signup(request, *args, **kwargs):
@@ -30,6 +20,7 @@ def signup(request, *args, **kwargs):
     params.is_valid(raise_exception=True)
     return JsonResponse(user_service.create(**params.data), status=status.HTTP_201_CREATED)
 
+@execption_hanlder()
 @api_view(['POST'])
 @parser_classes([JSONParser])
 def login(request, *args, **kwargs):
